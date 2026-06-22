5115c4325d036e3d71dac157bb666a209837a749
edurech901 <eduardo.rech901@gmail.com>
2026-06-21 23:20:41 -0300

    adicionando log pra ver as variaveis de ambiente

--- segundobi/assinafacil/netlify/functions/send-email.ts
+++ segundobi/assinafacil/netlify/functions/send-email.ts
@@ -64,6 +64,12 @@ const handler: Handler = async (event: HandlerEvent) => {
     };
   }
 
+  console.log("Debug SMTP Config:", {
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    user: process.env.SMTP_USER,
+  });
+
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT ?? 587),
